1a13a6a7540b43a1b877a479507b6886b36c718f
rad <conradsadler2@gmail.com>
2026-03-23 17:22:18 -0600

    Tested proxy contract against expected behavior. Also tested against malicious behavior.

---
+++ factoryTest.sol
@@ -0,0 +1,78 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+/// @notice The following contract is used purley to test proxy and factory contract capabilities
+contract testDAO
+{
+    uint256 accumulatedInput;
+    uint256 closingTime;
+    uint256 contract_id;
+    bool isFueled;
+    address proxy;
+    mapping (address => uint256) weiGiven;
+    event Refund(address indexed to, uint value);
+
+    constructor(uint256 _closingTime, uint256 _contract_id, address _proxy)
+    {
+        closingTime = _closingTime;
+        contract_id = _contract_id;
+        proxy = _proxy;
+    }
+
+	receive() external payable 
+    {
+        accumulatedInput += msg.value;
+    }
+
+	fallback() external payable 
+    {
+        accumulatedInput += msg.value;
+    }
+
+	function getBalance() public view returns(uint256)
+	{
+	    return address(this).balance;
+	}
+	function getProxy() public view returns(address)
+	{
+	    return proxy;
+	}
+
+	function deposit() public payable
+    {
+        weiGiven[msg.sender] += msg.value;
+        accumulatedInput += msg.value;
+    }
+    /// @notice VULNERABLE anyone can take contract balance
+	function refund() public payable
+        {
+            (bool success,) = msg.sender.call{value:  address(this).balance}("");
+            require(success, "Refund failed");
+	    emit Refund(msg.sender,address(this).balance);
+        }
+
+    ///@notice This function will only succeed if called by the user who generated the powned contract. Technically, anyone can drain the vulnerable Smart Contract of funds and then the user who created the contract can get the points
+	function powned() public
+    {
+        require(address(this).balance == 0,"address balance is not 0" );
+        (bool success, ) = proxy.call(abi.encodeWithSignature("increment_score(uint256,address)", contract_id, msg.sender));  //NOTE: there must not be spaces in abi.encode
+        require(success, "Not able to call proxy contract");
+	}
+}
+
+
+contract factoryTest is Ownable
+{
+    ///@notice This contract and all factory contracts are owned by the proxy contract. Only the proxy contract should be able to create a new clone of the vulnerable contract
+    constructor()  Ownable(0x5FbDB2315678afecb367f032d93F642f64180aa3){}
+    ///@notice This function is only callabe by the proxy contract and creates a new testDAO smart contract with a time stamp of 5min from present(not used), the contract identifyer of 1, and the proxy contract address
+    ///@notice All value sent to function by proxy is passed onto the newly created vuln contract
+    function create() external onlyOwner payable returns (address)
+    {
+        testDAO newReEntrancy = new testDAO(block.timestamp+300,1,0x5FbDB2315678afecb367f032d93F642f64180aa3); //passing the current time + 5 minutes and the vuln_id of 1 to the newly created vuln contract's contructor
+        newReEntrancy.deposit{value:  msg.value}(); //deposit function in vulnerable contract
+        return address(newReEntrancy);
+    }
+
+}
--- proxy.sol
+++ proxy.sol
@@ -2,8 +2,9 @@
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 pragma solidity ^0.8.22;
-
-contract proxy is Ownable{
+/// @notice This contract only supports 30 vulnerable smart contracts. This capacity could be increased by changing where the free contract generation bit is stored
+contract proxy is Ownable
+{
 
     // A table of user address to user stats
     mapping (address => uint256) public user_stats;
@@ -16,11 +17,22 @@ contract proxy is Ownable{
 
     uint128 public price_for_slot; // if public, there will be a getter for it impicitly included in the signiture
 
-    constructor(uint256 id, address first_contract, uint128 price) Ownable(msg.sender){
-        vuln_gen[id] = first_contract;
+    event balance(uint256 amount);
+
+    /// @notice making the proxy contract ownable so that only the maker of the contract can add new vulnerable contracts
+    constructor(uint128 price) Ownable(msg.sender)
+    {
         price_for_slot = price;
     }
 
+    receive() external payable 
+    { 
+        emit balance(address(this).balance);
+    }
+
+    /// @notice This function is only callable by owner
+    /// @param id must be a power of 2 for scoring system
+    /// @param _price_for_slot if price per slot does not need to change, then pass in existing price for slot
     function add_contract(uint256 id, address contract_address, uint128 _price_for_slot) external onlyOwner
     {
         require(vuln_gen[id] == address(0),"A contract is already mapped to this id");
@@ -29,39 +41,44 @@ contract proxy is Ownable{
         price_for_slot = _price_for_slot;
     }
 
+    /// @notice This function removes free cotract tracking bit to extract score
+    /// @param user is the address of the user you want to look up
     function get_score(address user) external view returns (uint256)
     {
         uint256 score = user_stats[user];
         require(score!= 0, "The user does not exist");
-        return 2147483647 & score; //(2**31) -1 mask b/c 2**32 should be plenty big for scores
+        return 2147483647 & score; //(2**31) -1 mask b/c 31 bit used to indicate free contract creation
     }
-    
-    function generate_contract(uint256 id) public payable
+
+    /// @notice This function will call the nessesary factory contract which will make a new vulnerable contract of type specified by id
+    /// @param id used to specify the contract to be generated
+    function generate_contract(uint256 id) public returns(address)
     {
         require(vuln_gen[id] != address(0),"The contract does not exist");
         uint256 stats = user_stats[msg.sender];
 
         //require(stats & id ==0, "You have already completed this contract"); #There is no harm in redoing a contract, they just cant increase their score
-        bool free_contract = ((stats & 2147483648) == 0); 
-
-        if(free_contract)
-        {
-            require(msg.value == 0); // the user should not pay if they are able to use a free contract
-            user_stats[msg.sender] = stats + 2147483648;
-        }
-        else
-        {
-            require(msg.value  == price_for_slot);
-        }
-        (bool success, bytes memory data) = vuln_gen[id].call{value:  1000 gwei}(abi.encodeWithSignature("create()"));
-        require(success, "Contract creation failed"); //prevents gas greiving
-        current_contract[msg.sender] = abi.decode(data, (address)); // data must be an address, if a user starts new contract before finishing the previous contract, they the incomplete contract will be lost
-
+        require((stats & 2147483648) == 0, "Free creation already used, send amount of price_for_slot to reset() or finish created contract before making a new contract."); 
+        require(address(this).balance >= 10000, "Contract does not have 10000 wei.");
+        user_stats[msg.sender] = stats + 2147483648;
+        (bool success, bytes memory data) = vuln_gen[id].call{value:  10000 }(abi.encodeWithSignature("create()"));
+        require(success, "Contract creation failed");
+        current_contract[msg.sender] = abi.decode(data, (address)); // data must be an address; if a user starts new contract before finishing the previous contract, they the incomplete contract will be lost
+        return abi.decode(data, (address));
     }
 
-    function increment_score(uint256 vuln_id, address player) public
+    function reset() public payable returns(bool)
     {
-        require(msg.sender == current_contract[player], "You are not working on this contract");
+        require(msg.value == price_for_slot);
+        uint256 stats = user_stats[msg.sender];
+        require(stats >= 2147483648, "No reason to pay reset() function");
+        user_stats[msg.sender] = stats - 2147483648;
+        return true;
+    }
+
+    function increment_score(uint256 vuln_id, address player) public returns(uint256)
+    {
+        require(msg.sender == current_contract[player], "Only vulnerable contract mapped to user can call this function");  //the msg.sender must be the vulnerable contract mapped to the users address in current_contract mapping
         uint256 stats = user_stats[player];
         if(stats & vuln_id == 0)
         {
@@ -69,9 +86,11 @@ contract proxy is Ownable{
         }
         else
         {
-            //per function generate_contract all users will have 2147483648 added to their score, so no risk of underflow
-            user_stats[player] = stats - 2147483648; //yes, even players that payed for the contract get their free contract reset, this means that someone could expend system resources by not completing,completing,notcompleting,...
+	        require(stats > 2147483647, "No reason to decriment score");
+            //per function generate_contract all users will have 2147483648 added to their score, so no risk of underflow unless they try to call increment score multipule times. The require above handles this case
+            user_stats[player] = stats - 2147483648; //yes, even players that payed for the contract get their free contract reset, this means that someone could expend system resources by not completing,completing,not completing,...
         }
+	    return user_stats[player];
     }
 
 }
